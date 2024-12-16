5641cb116534333bb37872a7be3572db5b12180a
Matias Chavez <mtschvz@gmail.com>
2024-12-15 21:17:54 -0300

    Primer commit

--- jarvis.py
+++ jarvis.py
@@ -12,7 +12,7 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 engine.setProperty('rate',130)
-
+"prueba"
 
 def talk(text):
     engine.say(text)
